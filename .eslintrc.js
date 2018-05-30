0868c39e24b96447cb4e7f823036d8c09d02c767
mikey <manicdesigner@gmail.com>
2018-05-30 11:32:35 -0400

    update eslintrc. rules I want for javascript 2018-05-30

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "mocha": true
   },
   "plugins": ["mocha"],
-  "extends": "eslint:recommended",
   "parserOptions": {
     "sourceType": "module"
   },
@@ -28,11 +27,34 @@ module.exports = {
       "error",
       "always"
     ]
-  }
+  },
+    "no-console": ["off", { allow: ["warn", "error"] }],
+    "no-control-regex": "off",
+    "no-debugger": "off", //, "error" -> turn off while working then on in dev.
+    "no-irregular-whitespace": [{"skipStrings": true,"skipComments": true,"skipRegExps": true}],
+    "block-scoped-var": "error",
+    "curly": "error",
+    "dot-notation": ["error", {"allowKeyWords": false }],
+    "eqeqeq": "error",
+    "guard-for-in": "error",
+    "no-alert": "warning" // "error" in production
+    "no-fallthrough": "error",
+    "no-floating-deciaml": "error",
+    "no-return-assign": "error", // EXCEPT IN PARENTHESES
+    "yoda": ["error"{"onlyEquality": true, "exceptRange": true }],
+    "strict": ["error", "function"],
+    "block-spacing": "error" // block spacing one before and after inside { }.
+    "brace-style": ["1tbs", { "allowSingleLine": true }],
+    "camelcase": ["error", { "properties": "never" }],
+    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
+    "no-tabs": "error",
+    "one-var": ["error", {"initalized": "always"}],
+    "padding-line-between-statements": [{ blankLine: "always", prev: "*", next: "return", blankLine: "always", prev: ["const", "let", "var"], next: "*"}, { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"], blankLine: "always", prev: "directive", next: "*" }, { blankLine: "any", prev: "directive", next: "directive"}],
+    "quotes": ["error", "single" {"allowTemplateLiterals": true}],
+    "semi": "error"
+
+
+
 };
-//!! no-console: ["off", { allow: ["warn", "error"] }] put in custom.
-// no-control-regex: "off"
-// no-debugger: "off", "error" -> turn off while working then on in dev.
-// no-irregular-whitespace: [{"skipStrings": true,"skipComments": true,"skipRegExps":
-// true,"skipTemplates": true}]
+
 
