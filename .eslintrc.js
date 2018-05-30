3360b268563a5454fea39e93f1d00132bcaf641d
mikey <manicdesigner@gmail.com>
2018-05-29 22:39:36 -0400

    reduce typing send people to a site for rules if they need to know what is in the extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,9 @@ module.exports = {
 
   "extends": "eslint:recommend",
   // extends:"eslint:recommend" -> relates to possible syntax or logic errors in js code.
-  /*1.for-direction: "error" -> error if for-loop moves in wrong dir infin. loop
-  | 2. getter-return: "error" -> all getters must have returns.
-  | 3. no-compare-neg-zero: true -> -0 will pass for +0 or -0 rid confusion.
-  | 4. no-cond-assign: "except-parens" -> allows assign only if closed in
-    // parentheses. "always" -> disallows ALL assignment in test conditions.
-  | 5. no-constant-condition: ["error:, {"checkLoops": true}] -> "true" allows constant
-  / expressions in loops.
-  | 6. no-dupe-args: "error" -> no duplicate arguments.
-  | 7. no-dupe-keys: "error" -> no duplicate keys.
-  | 8. no-duplicate-case: "error" -> no duplicate test expressions in cases of switch.
-  | 9. no-empty: "error" -> disallows empty block ststaments...unless a comments is there.
-  | 10. no-empty-character-class: "error" -> reg expressiosn no match anything are typos.
-  | 11. no-ex-assign: "error" -> cant reassign exception in catch clause.
-  | 12. no-extra-boolean-cast: "error" -> cant double negative KISS.
-  | 13. no-extra-semi: "error" -> no extra semi's on stuff.
-  | 14. no-func-assign: error -> must write a function expressions...function declarations bad.
-  | 15. no-inner-declarations: "error" -> func declartion not good inside things.
-  | 16. no-invalid-regexp: error -> invalid patterns give syntax errors.
-  | 17.
-  */
+  /* to find out what is in the extends: "eslint:recommend"
+     go to this web site https://eslint.org/docs/rules/
+   */
   "env" : {
     "browser": true,
     "es6": true,
