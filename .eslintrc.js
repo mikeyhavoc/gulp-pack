9b1f695b00ee66ad616a983dd2e17525fcfd6108
mikey <manicdesigner@gmail.com>
2018-05-31 11:52:52 -0400

    fixed eslint mostly.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,33 +7,33 @@ module.exports = {
      "no-console": ["off", { allow: ["warn", "error"] }],
      "no-control-regex": "off",
      "no-debugger": "off", //, "error" -> turn off while working then on in dev.
-     "no-irregular-whitespace": [{"skipStrings": true,"skipComments": true,"skipRegExps": true}],
+     "no-irregular-whitespace": ["warn", {skipStrings: true,skipComments: true,skipRegExps: true}],
      "block-scoped-var": "error",
      "curly": "error",
-     "dot-notation": ["error", {"allowKeyWords": false }],
+     "dot-notation": "error",
      "eqeqeq": "error",
      "guard-for-in": "error",
-     "no-alert": "warning", // "error" in production
+     "no-alert": "warn", // "error" in production
      "no-fallthrough": "error",
      "no-floating-deciaml": "error",
-     "no-return-assign": "error", // EXCEPT IN PARENTHESES
-     "yoda": ["error",{"onlyEquality": true, "exceptRange": true }],
+     "no-return-assign": "error",
+     "yoda": "error",
      "strict": ["error", "function"],
      "block-spacing": "error", // block spacing one before and after inside { }.
-     "brace-style": ["1tbs", { "allowSingleLine": true }],
+     "brace-style": "error",
      "camelcase": ["error", { "properties": "never" }],
      "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
      "no-tabs": "error",
-     "one-var": ["error", {"initalized": "always"}],
-     "padding-line-between-statements": [{ blankLine: "always", prev: "*", next: "return", blankLine: "always", prev: ["const", "let", "var"], next: "*"}, { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"], blankLine: "always", prev: "directive", next: "*" }, { blankLine: "any", prev: "directive", next: "directive"}],
-     "quotes": ["error", "single", {"allowTemplateLiterals": true}],
+     "one-var": ["error", {initialized : "always"}],
+     "padding-line-between-statements": "warn",
+     "quotes": ["error", "single", {allowTemplateLiterals: true}],
      "semi": "error",
-     "space-in-parens": ["error", {"exceptions": ["{}"]}],
+     "space-in-parens": ["error", "never", {"exceptions": ["{}"]}],
      "spaced-comment": ["error", "always"],
      "arrow-body-style": ["error", "as-needed"], // curlys {} only when needed.
-     "no-duplicate-imports": ["error", {"includeEports": true}],
+     "no-duplicate-imports": ["error", {includeExports: true}],
      "no-var": "error",
-     "prefer-template": "warning"
+     "prefer-template": "warn"
    }
 
 };
