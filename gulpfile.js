0c3f2eb3137e9c651e50c4ea1836d6c2302e0614
mikey <manicdesigner@gmail.com>
2018-06-01 10:41:14 -0400

    paths added for sass files, js files, and extra html pages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 const gulp = require('gulp');
 
-
+// example gulp hi test.
 gulp.task('hi', () => {
   console.log('hello');
 });
+
+
+const paths = {
+  styles: {
+    src: 'src/styles/**/*.sass',
+    dest: 'dest/assets/styles/'
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'dest/assets/scripts/'
+  },
+  pages: {
+    src: 'src/html/**/*.html',
+    dest: 'dest/html/'
+  }
+};
+
