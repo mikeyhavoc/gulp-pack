818b346dffc721a8b152bb9bb422c3586f0e4bd0
Michael Williams <manicdesigner@gmail.com>
2018-10-04 19:12:35 -0400

    requires for new gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,28 @@
+/** global requires */
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sourcemap = require('gulp-sourcemaps');
+const chalk = require('chalk');
+const log = require('fancy-log');
+const newer = require('gulp-newer');
+const plumber = require('gulp-plumber');
+const size = require('gulp-size');
+
+// css requires
+const cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass');
+const cached = require('gulp-cached');
+
+// JS requires ---------------------
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+// banner and pkg info for banner.
+const banner = require('moment');
+import pkg from './package';
+
+const gitinfo = gitInformation();
+
 
 // example gulp hi test.
 gulp.task('hi', () => {
@@ -21,3 +45,14 @@ const paths = {
   }
 };
 
+const banner = [
+  "/**",
+  " * @project       " + pkg.name ,
+  " * @author        " + pkg.author ,
+  " * @build          " + moment().format("llll") + " ET",
+  " * @release        " + gitinfo.abbreviatedSha + " [" + gitinfo.branch + "]",
+  " * @copyright      Copyright (c) " + moment().format("YYYY") + " " +  pkg.license ,
+  " *",
+  " */",
+  ""
+].join("\n");
